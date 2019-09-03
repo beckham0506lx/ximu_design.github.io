e721a8914b8f9ede963adb867963489a1d7a8025
beckham0506lx <beckxiang@foxmail.com>
2019-09-03 22:03:14 +0800

    12
    23

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["BECK.", "UI Designer.", "Graphic Designer.", "Freelancer.", "Photographer"],
       typeSpeed: 100,
       loop: true,
     });
